831972e2470835af30f65ba07b373935e99a234f
Sonakshi-Pan <75524755+Sonakshi-Pan@users.noreply.github.com>
2021-02-14 22:54:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var drops = []
+var man;
+var maxDrops=100;
+
+var thunderCreatedFrame=0;
+var thunder, thunder1,thunder2,thunder3,thunder4;
+
+function preload(){
+    thunder1 = loadImage("thunderbolt/1.png");
+    thunder2 = loadImage("thunderbolt/2.png");
+    thunder3 = loadImage("thunderbolt/3.png");
+    thunder4 = loadImage("thunderbolt/4.png");
+}
+
+function setup(){
+  createCanvas(500,700)  
+  engine = Engine.create();
+  world = engine.world;
+
+  man = new Man(200,500);
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new Drop(random(0,500), random(0,500), 3));
+        }
+
+    }
+/*
+ drops = new Drop(100,200,5)
+ man= new Man(140,200,500)
+ */
+
+
+}
+
+
+function draw(){
+  background(0)
+  rectMode(CENTER)
+   Engine.update(engine); 
+   var rand = Math.round(random(1,4));
+   if(frameCount%80===0){
+       thunderCreatedFrame=frameCount;
+       thunder = createSprite(random(10,470), random(10,30), 10, 10);
+       switch(rand){
+           case 1: thunder.addImage(thunder1);
+           break;
+           case 2: thunder.addImage(thunder2);
+           break; 
+           case 3: thunder.addImage(thunder3);
+           break;
+           case 4: thunder.addImage(thunder4);
+           break;
+           default: break;
+       }
+       thunder.scale = random(0.3,0.6)
+   }
+
+   if(thunderCreatedFrame + 10 ===frameCount && thunder){
+       thunder.destroy();
+   }
+
+   man.display();
+
+   //displaying rain drops
+   for(var i = 0; i<maxDrops; i++){
+       drops[i].display();
+       drops[i].updateRain()
+       
+   }
+   drawSprites()
+  
+}   
+
